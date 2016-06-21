ef05af35c34bbe2e63c6b294c0102c01781e135b
KURWINDALLAS <ddearcj@gmail.com>
2016-06-21 22:02:45 +0300

    initial

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-
+'use strict';
 var grouper = require('./GC_Grouper');
 
 var resource = "https://www.slimstore.com.ua";
 var fs = require('fs');
 
+const sourceFromDisk = 1;
+const sourceFromPhantom = 2;
+
+var mode = sourceFromDisk;
 
 var mysql      = require('mysql');
 
@@ -18,34 +22,37 @@ var connection = mysql.createConnection({
 var cheerio = require("cheerio");
 var request = require("request");
 
-var $ = cheerio.load(fs.readFileSync('./templates/test.html'));
-var bod = $('body');
-
-var phantom = require('phantom');
-phantom.create().then(function(ph) {
-	return ph.createPage().then(function(page) {
-		return page.open(resource).then(function(status) {
 
 
-				page.evaluate(function() {
-					window.scrollBy( 0, 10000 );
+if (mode == sourceFromPhantom) {
+	var phantom = require('phantom');
+	phantom.create().then(function (ph) {
+		return ph.createPage().then(function (page) {
+			page.open(resource).then(function (status) {
+				console.log(page.content);
+				page.evaluate(function () {
+					window.scrollBy(0, 10000);
 					return window.pageYOffset;
-				}).then(function(r){
-					page.content;
-					console.log(r);
+				}).then(function (r) {
+					var x = page.property('content').then(function (content) {
+						var $ = cheerio.load(content);
+						var bod = $('body');
+						callClassificator(bod);
+					});
 				});
+			});
 		});
 	});
-});
+}
 
 
-grouper.updateInfoTree(bod[0]);
-
-var element1 = $('.col-smb-12')[0];
-var element2 = $('.col-xs-12')[1];
-console.log(grouper.el2elComparsion(element1, element2));
 
 
+if (mode == sourceFromDisk) {
+	var $ = cheerio.load(fs.readFileSync('./templates/test.html'));
+	var bod = $('body');
+	callClassificator(bod);
+}
 
 
 request({
@@ -61,4 +68,15 @@ http.get(options, function(res, b) {
 	console.log("Got error: " + e.message);
 });
 */
+
+function callClassificator(body) {
+
+	grouper.updateInfoTree(body[0]);
+	var element1 = $('.col-smb-12')[0];
+	var element2 = $('.col-xs-12')[1];
+
+	console.log(grouper.el2elComparsion(element1, element2));
+}
+
+
 
