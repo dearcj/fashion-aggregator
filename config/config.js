4d71a1fb9ffc8c995aea78b6110ad6c1a6d58455
KURWINDALLAS <ddearcj@gmail.com>
2016-07-12 00:25:51 +0300

    initial

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
 
   pgconnection: {
+    host: '5.200.55.232',
     user: 'postgres',
     database: 'vagr',
-    password: 'root',
+    password: 'vagr123',
     port: 5432,
     max: 10,
     idleTimeoutMillis: 30000,
--- server.js
+++ server.js
@@ -21,14 +21,6 @@ var mode = sourceFromDisk;
 
 var pg = require('pg');
 
-var config = {
-	user: 'postgres',
-	database: 'vagr',
-	password: 'root',
-	port: 5432,
-	max: 10,
-	idleTimeoutMillis: 30000,
-};
 
 var pool = new pg.Pool(config.pgconnection);
 
