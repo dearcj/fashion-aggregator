ce47d86266883730019ce6a0c43fb6fcf1396116
yuriy <ddearcj@gmail.com>
2016-08-15 02:18:09 +0300

    dictionary with data fix

--- Backend/App.js
+++ Backend/App.js
@@ -102,31 +102,32 @@ var App = (function () {
         phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']).then(function (ph) {
             var pg = ph.createPage();
             return pg.then(function (page) {
-                page.property('viewportSize', { width: 1024, height: 768 });
+              page.property('viewportSize', {width: 1024, height: 768}).then(function () {
                 page.property('customHeaders', {}).then(function () {
-                    page.property('customHeaders', {
-                        'Host': 'localhost:1337',
-                        'Connection': 'keep-alive',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-                        'Accept-Encoding': 'gzip, deflate, sdch',
-                        'Accept-Language': 'en-US,en;q=0.8,ru;q=0.6'
-                    }).then(function () {
-                        page.on('onResourceRequested', function (requestData, networkRequest, obj) {
-                            urls.push(requestData.url); // this would push the url into the urls array above
-                            obj.push('asdasdsa');
-                            console.log('Request ' + JSON.stringify(request, undefined, 4));
-                        }, obj);
-                        page.open(url).then(function (status) {
-                            //console.log(page.content);
-                            page.evaluate(function () {
-                                window.scrollBy(0, 10000);
-                                return window.pageYOffset;
-                            }).then(function (r) {
-                                var x = page.property('content').then(function (content) {
-                                    var $ = cheerio.load(content);
-                                    var bod = $('body');
-                                    endCB(bod[0]);
+                  page.property('customHeaders', {
+                    'Host': 'localhost:1337',
+                    'Connection': 'keep-alive',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+                    'Accept-Encoding': 'gzip, deflate, sdch',
+                    'Accept-Language': 'en-US,en;q=0.8,ru;q=0.6'
+                  }).then(function () {
+                    page.on('onResourceRequested', function (requestData, networkRequest, obj) {
+                      urls.push(requestData.url); // this would push the url into the urls array above
+                      obj.push('asdasdsa');
+                      console.log('Request ' + JSON.stringify(request, undefined, 4));
+                    }, obj);
+                    page.open(url).then(function (status) {
+                      //console.log(page.content);
+                      page.evaluate(function () {
+                        window.scrollBy(0, 10000);
+                        return window.pageYOffset;
+                      }).then(function (r) {
+                        var x = page.property('content').then(function (content) {
+                          var $ = cheerio.load(content);
+                          var bod = $('body');
+                          endCB(bod[0]);
+                        });
                                 });
                             });
                         });
