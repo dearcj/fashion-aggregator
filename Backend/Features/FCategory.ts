8d3aba1482e36fd880c16e4a04782df165e57ef9
Yuriy Dorogoy <ddearcj@gmail.com>
2016-08-18 12:07:45 +0300

    fcategory fix

--- Backend/Features/FCategory.ts
+++ Backend/Features/FCategory.ts
@@ -1,6 +1,13 @@
 import { Feature } from "./Feature";
+import {DOMObject} from "../GC_Grouper";
 
 export class FCategory extends Feature {
+
+  extractValue(e:DOMObject) {
+    if (!e)
+      console;
+    return e.data;
+  }
 
   addChildCategory(category, parentId): void {
     this.dict.addWord(category, true, parentId.toString());
